d191c63b4d27c245b61d88cc05a6e678e14d491d
Javi <jperez7@nigul.cide.es>
2019-11-18 19:33:37 +0100

    Tercer Commit

--- src/nombretoguapo/cosa.java
+++ src/nombretoguapo/cosa.java
@@ -3,6 +3,10 @@ package nombretoguapo;
 public class cosa {
 
 	public static void main(String[] args) {
+		printHola();
+	}
+	
+	public static void printHola() {
 		System.out.println("Hola món!");
 	}
 
